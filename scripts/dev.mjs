b792d62b0c31c66ef1115a6b35bae16647c3e12e
Arnav Gosain <arnav@arnavgosain.com>
2021-06-19 16:55:53 +0530

    Add prettier

--- scripts/dev.mjs
+++ scripts/dev.mjs
@@ -8,46 +8,45 @@ import chalk from "chalk";
 import chokidar from "chokidar";
 import config, { outPath, __dirname } from "./config.mjs";
 
+const srcPath = path.resolve(__dirname, "../src");
 const popupPath = path.resolve(__dirname, "../src/popup.js");
 const contentScriptPath = path.resolve(__dirname, "../src/content_script.js");
 const publicPath = path.resolve(__dirname, "../public");
 
 console.log(`--- BUILDING IN WATCH MODE ---`);
 
-chokidar
-  .watch([popupPath, contentScriptPath, publicPath], {})
-  .on("all", (_, triggerPath) => {
-    if (triggerPath.includes("public")) {
-      fse.copySync(publicPath, outPath, {
-        overwrite: true,
-      });
-    }
+chokidar.watch([srcPath, publicPath], {}).on("all", (_, triggerPath) => {
+  if (triggerPath.includes("public")) {
+    fse.copySync(publicPath, outPath, {
+      overwrite: true,
+    });
+  }
 
-    if (triggerPath.includes("popup.js")) {
-      console.log(chalk.green("Detected change, rebuilding popup.js"));
-      esbuild
-        .build({
-          ...config,
-          entryPoints: {
-            popup: popupPath,
-          },
-          plugins: [sveltePlugin(), postcssPlugin()],
-        })
-        .catch(console.error);
-    }
+  if (triggerPath.includes("popup")) {
+    console.log(chalk.green("Detected change, rebuilding popup.js"));
+    esbuild
+      .build({
+        ...config,
+        entryPoints: {
+          popup: popupPath,
+        },
+        plugins: [sveltePlugin(), postcssPlugin()],
+      })
+      .catch(console.error);
+  }
 
-    if (triggerPath.includes("content_script.js")) {
-      console.log(chalk.green("Detected change, rebuilding content_script.js"));
-      esbuild
-        .build({
-          ...config,
-          entryPoints: {
-            content_script: contentScriptPath,
-          },
-        })
-        .then((result) => {
-          if (result.errrors) console.log(chalk.red(result.errors));
-        })
-        .catch(console.error);
-    }
-  });
+  if (triggerPath.includes("content_script.js")) {
+    console.log(chalk.green("Detected change, rebuilding content_script.js"));
+    esbuild
+      .build({
+        ...config,
+        entryPoints: {
+          content_script: contentScriptPath,
+        },
+      })
+      .then((result) => {
+        if (result.errrors) console.log(chalk.red(result.errors));
+      })
+      .catch(console.error);
+  }
+});
--- src/popup/App.svelte
+++ src/popup/App.svelte
@@ -1,4 +1,5 @@
 <script>
+  let name = "Hello";
 </script>
 
-<main>Hello</main>
+<main>{name}</main>
